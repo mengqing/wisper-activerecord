7dd2ff2ee76ec18f1a84594d04c40550a20e49b8
Jimmy Zhang <mengqing@gmail.com>
2014-07-12 00:04:42 +1000

    added failed broadcasting

--- lib/wisper/activerecord/publisher.rb
+++ lib/wisper/activerecord/publisher.rb
@@ -9,21 +9,35 @@ module Wisper
         after_commit :publish_creation, on: :create
         after_commit :publish_update, on: :update
         after_commit :publish_destroy, on: :destroy
-
+        after_validation :publish_creation_failed, on: :create
+        after_validation :publish_update_failed, on: :update
+        after_validation :publish_destroy_failed, on: :destroy
       end
 
       private
 
       def publish_creation
-        broadcast(:on_create, self)
+        broadcast("#{self.class.name.underscore}_created", self)
       end
 
       def publish_update
-        broadcast(:on_update, self)
+        broadcast("#{self.class.name.underscore}_updated", self)
       end
 
       def publish_destroy
-        broadcast(:on_destroy, self)
+        broadcast("#{self.class.name.underscore}_destroyed", self)
+      end
+
+      def publish_creation_failed
+        broadcast("#{self.class.name.underscore}_create_failed", self) if errors.any?
+      end
+
+      def publish_update_failed
+        broadcast("#{self.class.name.underscore}_update_failed", self) if errors.any?
+      end
+
+      def publish_destroy_failed
+        broadcast("#{self.class.name.underscore}_destroy_failed", self) if errors.any?
       end
     end
   end
--- spec/wisper/activerecord_spec.rb
+++ spec/wisper/activerecord_spec.rb
@@ -5,18 +5,31 @@ describe 'ActiveRecord' do
 
   describe 'create' do
 
-    it 'publishes an on_create event to listener' do
-      expect(listener).to receive(:on_create)
+    it 'publishes an created event to listener' do
+      expect(listener).to receive(:meeting_created)
       Wisper::Activerecord.subscribe(Meeting, to: listener)
 
       Meeting.create!
     end
 
     it 'publishes created object to listener' do
-      expect(listener).to receive(:on_create).with(an_instance_of(Meeting))
+      expect(listener).to receive(:meeting_created).with(an_instance_of(Meeting))
       Wisper::Activerecord.subscribe(Meeting, to: listener)
 
       Meeting.create!
+    end
+
+    it 'publishes an created failed event to listener if failed creation' do
+      expect(listener).to receive(:meeting_create_failed)
+      Wisper::Activerecord.subscribe(Meeting, to: listener)
+
+      meeting = Meeting.new
+      meeting.singleton_class.class_eval do
+        attr_accessor :name
+        validates_presence_of :name
+      end
+
+      meeting.save
     end
 
   end
@@ -24,21 +37,35 @@ describe 'ActiveRecord' do
   describe 'update' do
     before { Meeting.create! }
 
-    it 'publishes an on_update event to listener' do
-      expect(listener).to receive(:on_update)
+    it 'publishes an updated event to listener' do
+      expect(listener).to receive(:meeting_updated)
       Wisper::Activerecord.subscribe(Meeting, to: listener)
 
       meeting = Meeting.first
       meeting.update_attributes(title: 'new title')
 
     end
+
+    it 'publishes an update failed event to listener if failed update' do
+      expect(listener).to receive(:meeting_update_failed)
+      Wisper::Activerecord.subscribe(Meeting, to: listener)
+
+      meeting = Meeting.first
+      
+      meeting.singleton_class.class_eval do
+        attr_accessor :name
+        validates_presence_of :name
+      end
+
+      meeting.update_attributes(title: 'new title')
+    end
   end
 
   describe 'destroy' do
     before { Meeting.create! }
 
-    it 'publishes an on_destroy event to listener' do
-      expect(listener).to receive(:on_destroy)
+    it 'publishes a destroyed event to listener' do
+      expect(listener).to receive(:meeting_destroyed)
       Wisper::Activerecord.subscribe(Meeting, to: listener)
 
       meeting = Meeting.first
